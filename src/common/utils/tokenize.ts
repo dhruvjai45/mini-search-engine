63b62d65493329be8464fada060a008477170c81
dhruvjai45 <dhruvjain.gdsc@gmail.com>
2026-07-14 09:55:14 +0530

    Improve tokenization logic

--- src/common/utils/tokenize.ts
+++ src/common/utils/tokenize.ts
@@ -43,6 +43,8 @@ export function tokenize(
   } = options;
 
   const normalized = input
+    .normalize('NFKD')
+    .replace(/[\u0300-\u036f]/g, '')
     .toLowerCase()
     .replace(/[^a-z0-9\s]/g, ' ')
     .replace(/\s+/g, ' ')
